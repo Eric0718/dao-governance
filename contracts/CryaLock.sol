eb72ed37b31d23f88748710f843c1eafab4e27be
Eric0718 <1434797171@qq.com>
2022-09-15 19:44:07 +0800

    update code

--- contracts/CryaLock.sol
+++ contracts/CryaLock.sol
@@ -12,11 +12,9 @@ contract CryaLock{
 
     enum AddressType{
       SaftRound,
-      StrategicSupporter,
       Ecology,
       IDOPublicOffering,
       Consultant,
-      NftSale,
       Team
     }
 
@@ -61,10 +59,6 @@ contract CryaLock{
         distributionRatios[AddressType.SaftRound] = tokenTotalSupply.mul(15).div(100);
         distributionRatiosUsed[AddressType.SaftRound] = 0;
 
-        //StrategicSupporter is 16% of tokenTotalSupply.
-        distributionRatios[AddressType.StrategicSupporter] = tokenTotalSupply.mul(16).div(100);
-        distributionRatiosUsed[AddressType.StrategicSupporter] = 0;
-
         //Ecology is 39% of tokenTotalSupply.
         distributionRatios[AddressType.Ecology] = tokenTotalSupply.mul(39).div(100);
         distributionRatiosUsed[AddressType.Ecology] = 0;
@@ -76,10 +70,6 @@ contract CryaLock{
         //Consultant is 6% of tokenTotalSupply.
         distributionRatios[AddressType.Consultant] = tokenTotalSupply.mul(6).div(100);
         distributionRatiosUsed[AddressType.Consultant] = 0;
-
-        //NftSale is 5% of tokenTotalSupply
-        distributionRatios[AddressType.NftSale] = tokenTotalSupply.mul(5).div(100);
-        distributionRatiosUsed[AddressType.NftSale] = 0;
 
         //Team is 15% of tokenTotalSupply
         distributionRatios[AddressType.Team] = tokenTotalSupply.mul(15).div(100);
@@ -104,18 +94,13 @@ contract CryaLock{
         }
     }
 
-    //release locked balance by address type
-    function releaseLockedBalance(uint8 _type) public onlyAdmin{
-        require(_type >=0 && _type <=6,"Wrong type!");
+    //release locked balance when addresses need to release.
+    function releaseLockedBalance() public onlyAdmin{
         require(block.timestamp >= tgeTime,"TGE not start!");
         for (uint256 i = 0;i < addresses.length;i++){
-            address user = addresses[i];
-            require(addressInfos[user].releaseStartTime > 0 ,"Not a release address!");
-            if (_type == addressInfos[user].addressType){
-                require(block.timestamp >= addressInfos[user].releaseStartTime,"release not start!");
-                uint256 releaseAmount = calculateReleaseAmount(user);
-                require(releaseAmount >0,"Not a support type!");
-                release(user,releaseAmount);
+            uint256 releaseAmount = calculateReleaseAmount(addresses[i]);
+            if (releaseAmount >0){
+                release(addresses[i],releaseAmount);
             }
         }
     }
@@ -128,60 +113,58 @@ contract CryaLock{
         uint256 calTime = block.timestamp > endTime ? endTime : block.timestamp;
         uint256 releaseAmount;
 
+        if(calTime < updateTime || startTime == 0){
+            return 0;
+        }
+
         if (userType == uint8(AddressType.SaftRound)){
-            //release in 18 months
-            if(calTime >= updateTime){
-                if(addressInfos[user].totalLocked == addressInfos[user].lockedLeft){
-                    return addressInfos[user].totalLocked.mul(5).div(100); 
-                }
-                releaseAmount = addressInfos[user].totalLocked.div(18);
+            //TGE release 5%
+            if(addressInfos[user].totalLocked == addressInfos[user].lockedLeft){
+                return addressInfos[user].totalLocked.mul(5).div(100); 
+            }else{
+                //release 95% in 18 months
+                uint256 lockedBalance = addressInfos[user].totalLocked.mul(95).div(100);
+                releaseAmount = lockedBalance.div(18);
             }
         }else if (userType == uint8(AddressType.Ecology)){
-            if(calTime >= updateTime){
-                //25% locked release in 9 months 
-                if(calTime < (startTime + 10 * baseTimeInterval)){
-                    uint256 lockedBalance = addressInfos[user].totalLocked.mul(25).div(100);
-                    releaseAmount = lockedBalance.div(9);             
-                }else if (calTime > (startTime + 10 * baseTimeInterval)){    
-                    //75% locked release in 48  months
-                    uint256 lockedBalance = addressInfos[user].totalLocked.mul(75).div(100);
-                    releaseAmount = lockedBalance.div(48);
-                }
-            } 
+            //25% locked release in 9 months 
+            if(calTime < (startTime + 10 * baseTimeInterval)){
+                uint256 lockedBalance = addressInfos[user].totalLocked.mul(25).div(100);
+                releaseAmount = lockedBalance.div(9);             
+            }else{    
+                //75% locked release in 48  months
+                uint256 lockedBalance = addressInfos[user].totalLocked.mul(75).div(100);
+                releaseAmount = lockedBalance.div(48);
+            }
         }else if (userType == uint8(AddressType.IDOPublicOffering)){
-            if(calTime >= updateTime){
-                //TGE release 33.3%
-                if(addressInfos[user].totalLocked == addressInfos[user].lockedLeft){
-                    return addressInfos[user].totalLocked.mul(333).div(1000);
-                }else if (calTime > (updateTime + baseTimeInterval)){
-                    //66.7% release in two months
-                    uint256 lockedBalance = addressInfos[user].totalLocked.mul(667).div(1000);
-                    releaseAmount = lockedBalance.div(2);
-                }
-            }     
+            //TGE release 33.3%
+            if(addressInfos[user].totalLocked == addressInfos[user].lockedLeft){
+                return addressInfos[user].totalLocked.mul(333).div(1000);
+            }else{
+                //66.7% release in two months
+                uint256 lockedBalance = addressInfos[user].totalLocked.mul(667).div(1000);
+                releaseAmount = lockedBalance.div(2);
+            }    
         }else if (userType == uint8(AddressType.Consultant)){
             //release in 33 months
-            if(calTime >= updateTime){
-                releaseAmount = addressInfos[user].totalLocked.div(33);
-            }
+            releaseAmount = addressInfos[user].totalLocked.div(33);
         }else if (userType == uint8(AddressType.Team)){
-            if(calTime >= updateTime){
-                //20% release in a year
-                if(addressInfos[user].totalLocked == addressInfos[user].lockedLeft){
-                    return addressInfos[user].totalLocked.mul(20).div(100);
-                }else if (calTime > (updateTime + baseTimeInterval)){
-                    //80% release in 48 months
-                    uint256 lockedBalance = addressInfos[user].totalLocked.mul(80).div(100);
-                    releaseAmount = lockedBalance.div(48);
-                }
-            }  
+            //20% release in a year
+            if(addressInfos[user].totalLocked == addressInfos[user].lockedLeft){
+                return addressInfos[user].totalLocked.mul(20).div(100);
+            }else if (calTime > (updateTime + baseTimeInterval)){
+                //80% release in 48 months
+                uint256 lockedBalance = addressInfos[user].totalLocked.mul(80).div(100);
+                releaseAmount = lockedBalance.div(48);
+            }
         }else{
-            revert("Not a correct type to release!");
+            return 0;
         }
 
         uint256 numbs = (calTime - updateTime).div(baseTimeInterval);
-        require(numbs > 0,"Release: Not a correct time to release!");
-        addressInfos[user].lastUpdateTime = updateTime + numbs * baseTimeInterval;
+        if (numbs >0){
+            addressInfos[user].lastUpdateTime = updateTime + numbs * baseTimeInterval;
+        }
         return releaseAmount * numbs;
     }
 
